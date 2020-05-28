f890b3b664b32349bfc8d8255b52a85c66e93710
Raul Baltasar <r.baltasarb@gmail.com>
2020-05-28 16:16:35 +0200

    Added MQTT communication

--- garbage_calendar_pluggin/update_events.py
+++ garbage_calendar_pluggin/update_events.py
@@ -1,11 +1,11 @@
+import time
+import paho.mqtt.client as paho
 from icalendar import Calendar, Event
 from datetime import datetime
 import urllib3
 
-
-
 stuttgart_abfallkalendar_url = 'https://service.stuttgart.de/lhs-services/aws/api/ical?street=Mathildenstr.&streetnr=7A'
-TIME_THRESHOLD = 5
+TIME_THRESHOLD = 50
 
 #--------------------------------------------#
 #Class process calendar event                #
@@ -58,28 +58,50 @@ class CalendarPlugIn:
                 elif(event_type == "Biomüll"):
                     self._notify_biomull = True
 
+    def OpenMQTTConnection(self):
+
+        #Create MQTT client
+        self._client = paho.Client("GarbagePluggin")
+        #Todo: IP address and port in config
+        self._client.connect("192.168.2.118",1883)
+        #Start client thread
+        self._client.loop_start()
+        print("Started MQTT client")
+        time.sleep(0.1)
+
     def ProcessEvents(self):
+
+        #Connect to MQTT broker
+        self.OpenMQTTConnection()
 
         if(self._notify_plastic):
             self.NotifyPlastic()
+            time.sleep(0.5)
         if(self._notify_papier):
             self.NotifyPapier()
+            time.sleep(0.5)
         if(self._notify_restmull):
             self.NotifyRestmull()
+            time.sleep(0.5)
         if(self._notify_biomull):
             self.NotifyBiomull()
+            time.sleep(0.5)
 
     def NotifyPlastic(self):
         print("Notifying plastic")
+        self._client.publish("smart_garbage/gelber","1",1)
 
     def NotifyPapier(self):
         print("Notifying papier")
+        self._client.publish("smart_garbage/papier","1",1)
 
     def NotifyRestmull(self):
         print("Notifying restmull")
+        self._client.publish("smart_garbage/restmull","1",1)
 
     def NotifyBiomull(self):
         print("Notifying biomull")
+        self._client.publish("smart_garbage/biomull","1",1)
 
 
 if __name__== "__main__":
